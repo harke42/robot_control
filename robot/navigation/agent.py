1e4e353c0942ebc1c5fd4afa419a29575fbd02ae
jbaermann <j.baermann@campus.tu-berlin.de>
2024-11-06 12:24:42 +0100

    wip: navigation, movement implementation

--- robot/navigation/agent.py
+++ robot/navigation/agent.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from robot import VisionRobot
 
-SPEED_AT_1 = 206
+VELOCITY_AT_1 = 206
 
 INSTRUCTION_SET = ["goals", "movements"]
 USE_GOALS = 0
@@ -14,15 +14,17 @@ USE_MOVEMENTS = 1
 X = 0
 Y = 1
 
+DIST_BETWEEN_MOTORS = 108 # [mm]
+
 class Agent:
 
-    movements: list # movement structure: []
+    movements: list # movement structure: [time, dphi, radius]
     goal_position: list
 
     instruction_set: int
 
-    position: list # [x, y] in mm
-    phi: float # direction; -pi < phi < pi
+    position: list # [x, y] in [mm]
+    phi: float # direction in [rad]; -pi < phi < pi
 
     robot: VisionRobot
 
@@ -39,24 +41,92 @@ class Agent:
         self.instruction_set = instruction_set
         self.direction = start_dir
         self.robot = robot
+        if instruction_set == USE_MOVEMENTS:
+            self.update_thread = threading.Thread(target=self._movement_task)
 
     def start(self):
         self.robot.start()
+        self.update_thread.start()
 
-    def add_movement(self, time=-1, dphi=0, radius=-1):
+    def add_movement(self, time=-1, dphi=-1, radius=-1):
+        self.movements.append([time, dphi, radius])
+
+    def _movement_task(self):
+        while True:
+            try:
+                if len(self.movements) > 0:
+                    movement = self.movements.pop(0)
+                    self._calculate_movement(time=movement[0], dphi=movement[1], radius=movement[2])
+
+            except Exception as e:
+                print("Error in Movement Task!")
+                print(e)
+                break
+
+    #TBD: _goal_task()
+
+    def _speed_to_motor_input(self, velocity):
+        return velocity/VELOCITY_AT_1
+
+    def _vr_vl_from_radius(self, radius):
+        ratio_vr_vl = (2*radius + DIST_BETWEEN_MOTORS)/(2*radius - DIST_BETWEEN_MOTORS)
+        v_l = 0
+        v_r = 0
+        if ratio_vr_vl > 0:
+            # case v_r larger than v_l --> left turn
+            v_r = VELOCITY_AT_1
+            v_l = v_r/ratio_vr_vl
+        
+        else:
+            # case v_l larger than v_r --> right turn
+            v_l = VELOCITY_AT_1
+            v_r = ratio_vr_vl * v_l
+            
+        return v_l, v_r
+
+
+    def _calculate_movement(self, time=-1, dphi=-1, radius=-1):
+        """ time in [s], dphi in [rad], radius in [mm] """
+
+        if INSTRUCTION_SET != USE_MOVEMENTS:
+            print("Tried to add movement while in GOAL mode")
+            return
+        v_l = 0
+        v_r = 0
+        
         if dphi == 0 and radius == -1:
-            self.robot.setSpeed([1.0, 1.0])
-            time.sleep(time)
-            self.position[X] += np.sin(self.phi) * time * SPEED_AT_1
-            self.position[Y] += np.cos(self.phi) * time * SPEED_AT_1
-        elif dphi == 0 and time == -1:
-            dtime = radius/206
-            self.robot.setSpeed([1.0, 1.0])
-        
+            v_l = 1.0
+            v_r = 1.0
 
-    def _update_position(self):
-        ...
+        elif dphi == 0 and time == -1:
+            time = radius/206
+            v_l = 1.0
+            v_r = 1.0
+
+        elif time == -1 and radius != -1:
+            v_l, v_r = self._vr_vl_from_radius(radius)
+            time = (dphi*DIST_BETWEEN_MOTORS)/(v_r - v_l)
         
+        elif dphi == -1 and radius != -1:
+            v_l, v_r = self._vr_vl_from_radius(radius)
+            dphi = (v_r - v_l)*time * 1/DIST_BETWEEN_MOTORS
+            
+        else:
+            print("Uncompleted movement command, skipping!")
+            print("Input: time"+str(time)+" dphi=" + str(dphi) + " radius=" + str(radius))
+            return
+        
+        s_l = self._speed_to_motor_input(v_l)
+        s_r = self._speed_to_motor_input(v_r)
+        self.robot.setSpeed([s_l, s_r])
+        time.sleep(time)
+        self._update_position(dphi, v_l, v_r)
+
+    def _update_position(self, dphi, v_l, v_r):
+        self.position[X] += 0.5*(v_r + v_l)*(np.sin(self.phi + dphi) - np.sin(self.phi))
+        self.position[X] += 0.5*(v_r + v_l)*(-np.cos(self.phi + dphi) + np.cos(self.phi))
+        self.phi += dphi
+
 """
 SAMPLE_TIME = 0.01
 
